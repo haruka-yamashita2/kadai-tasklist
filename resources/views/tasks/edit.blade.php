6ed061f251a67b77043852da0fcb909af42feecb
haruka.yamashita <haruka-yamashita2@dac.co.jp>
2018-06-14 05:05:10 +0000

    bootstrap.tasklist

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,7 +6,7 @@
     
 <div class="row">
         <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-        </div>
+
         {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
             <div class="form-group">
                 {!! Form::label('status', 'ステータス:') !!}
